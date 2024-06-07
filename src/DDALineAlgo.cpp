251e36264e0e249e1c60873fb2ad892c5b5c39d0
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2024-06-07 20:23:31 +0545

    update: input, buffer, util based normalize

--- src/DDALineAlgo.cpp
+++ src/DDALineAlgo.cpp
@@ -1,9 +1,12 @@
 #include"./include_header.h"
+#include"../Util/Utils.cpp"
 
 namespace Extra
 {
 	#define SCR_WIDTH 800
-	#define SCR_HEIGHT 600
+	#define SCR_HEIGHT 800
+	#define POINT_SIZE 4.0
+	// #define INPUT_ENABLE
 
 	ApplicationInfo appInfo(SCR_WIDTH, SCR_HEIGHT, "DDA Line Algorithm");
 
@@ -19,27 +22,54 @@ namespace Extra
 
 		void Loop() override
 		{
-			float *quadVerts = get_quad_bufferind(fVec2(0), fVec2(0.5), Color3(0.5, 1.0, 0.2));
-			uint *quadIBO = get_quad_index();
+			// variable declaraiton for DDA algo
+			fVec2 x1y1, x2y2, del, inc;
+			#ifdef INPUT_ENABLE
+			x1y1 = Util::get_2dpoint("# Inital:");
+			x2y2 = Util::get_2dpoint("# Final:");
+			#else
+			x1y1 = fVec2(0, 0);
+			x2y2 = fVec2(SCR_HEIGHT/4, SCR_WIDTH);
+			#endif
+			del = fVec2(x2y2.x - x1y1.x, x2y2.y - x1y1.y);
 
-			VertexArrayObject VAO(BufferLayout::PPP_RGB);
+			int step = abs(del.x) > abs(del.y) ? abs(del.x) : abs(del.y);
+			inc = fVec2(del.x/step, del.y/step);
+			fVec2 xy = fVec2(x1y1.x, x1y1.y);
+
+			std::vector<float> plotpoint;
+			plotpoint.clear();
+
+			// Calculation of the intermediate points
+			while(step > 0)
+			{
+				xy = fVec2(xy.x + inc.x, xy.y + inc.y);
+				plotpoint.push_back(xy.x);
+				plotpoint.push_back(xy.y);
+				step--;
+			}
+
+			// normalize from scren space to -1 to 1
+			Util::normalizeM11_point2d(plotpoint.data(), plotpoint.size(), iVec2(SCR_WIDTH, SCR_HEIGHT));
+			print_point2d(plotpoint.data(), plotpoint.size(), '\n');
+
+			VertexArrayObject VAO(BufferLayout::PP);
 			VertexBufferObject VBO;
-			IndexBufferObject IBO;
 			VAO.Bind();
-			IBO.PushData(quadIBO, 6);
-			VBO.PushData(quadVerts, 24);
+			VBO.PushData(plotpoint.data(), plotpoint.size());
 			VBO.OffloadData();
-			IBO.OffloadData();
 			VAO.SetVertexAttributePointer();
 
-			Shader *temp = this->GetRawShader("../res/Shaders/");
+			Shader *temp = this->GetRawShader("../res/Shaders/line/");
 
+			// main loop 
 			while (!m_window->ShouldCloseWindow())
 			{
-				m_window->SetColor(0, 0, 0, 0);
+				m_window->SetColor(1, 1, 1, 0);
 				temp->UseProgram();	
 
-				glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+				glPointSize(POINT_SIZE);
+				glDrawArrays(GL_POINTS, 0, plotpoint.size()/2);
 
 				m_window->SwapFrameBuffer();
 				glfwPollEvents();
